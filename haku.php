de9088266de5e68c7273264c4419f9881a37aa9b
Ivan Lutrov <lutrov@users.noreply.github.com>
2017-04-05 10:30:41 +1000

    Update haku.php

--- haku.php
+++ haku.php
@@ -14,13 +14,14 @@ defined('ABSPATH') || die('Ahem.');
 //
 // Define constants used by this plugin.
 //
-define('HAKU_DATE_FORMAT', get_option('date_format'));
-define('HAKU_EXCERPT_WORD_COUNT', 80);
-define('HAKU_RESULTS_PAGE_SLUG', 'search');
-define('HAKU_SHOW_META', true);
-define('HAKU_SHOW_META_AUTHOR', true);
-define('HAKU_SHOW_META_DATE', true);
-define('HAKU_SHOW_SEARCH_FORM_ON_RESULTS_PAGE', true);
+define('HAKU_FORM_PLACEHOLDER_TEXT', 'Search for&hellip;');
+define('HAKU_SERP_DATE_FORMAT', get_option('date_format'));
+define('HAKU_SERP_EXCERPT_WORD_COUNT', 20);
+define('HAKU_SERP_SHOW_FORM', true);
+define('HAKU_SERP_SHOW_META', true);
+define('HAKU_SERP_SHOW_META_AUTHOR', true);
+define('HAKU_SERP_SHOW_META_DATE', true);
+define('HAKU_SERP_SLUG', 'search');
 
 //
 // Don't touch these unless you want the sky to fall.
@@ -40,11 +41,11 @@ function haku_search_query() {
 function haku_search_form($caption = null) {
 	static $form = null;
 	if (strlen($form) == 0) {
-		$slug = apply_filters('haku_results_page_slug_filter', HAKU_RESULTS_PAGE_SLUG);
+		$slug = apply_filters('haku_serp_slug_filter', HAKU_SERP_SLUG);
 		if ($page = get_page_by_path($slug)) {
 			$action = get_permalink($page->ID);
 			if (substr(trim($action, '/'), 0 - strlen($slug)) == $slug) {
-				$form = sprintf('<form method="post" action="%s" class="search-form haku-search-form" role="search"><input type="search" name="q" placeholder="%s" value="%s"><input type="submit" value="%s"></form>', $action, esc_attr__('Haku for&hellip;'), haku_search_query(), esc_attr__(strlen($caption) > 0 ? $caption : __('Search')));
+				$form = sprintf('<form method="post" action="%s" class="haku-form search-form" role="search"><input type="search" name="q" placeholder="%s" value="%s"><input type="submit" value="%s"></form>', $action, esc_attr__(apply_filters('haku_form_placeholder_text_filter', HAKU_FORM_PLACEHOLDER_TEXT)), haku_search_query(), esc_attr__(strlen($caption) > 0 ? $caption : __('Search')));
 			}
 		}
 	}
@@ -66,20 +67,20 @@ function haku_search_results() {
 		}
 		$in = haku_post_types();
 		$query = "SELECT $wpdb->posts.ID, $wpdb->posts.post_name, $wpdb->posts.post_title, $wpdb->posts.post_date, $wpdb->posts.post_author, $wpdb->posts.post_content, IF(LOCATE('$q', $wpdb->posts.post_title), 1, 0) AS IN_TITLE, IF(LOCATE('$q', $wpdb->posts.post_content), 1, 0) AS IN_CONTENT, LOCATE('$q', $wpdb->posts.post_title) AS TITLE_POS, LOCATE('$q', $wpdb->posts.post_content) AS CONTENT_POS, (LENGTH($wpdb->posts.post_title) - LENGTH(REPLACE(UPPER($wpdb->posts.post_title), '$q', ''))) / LENGTH('$q') AS TITLE_CNT, (LENGTH($wpdb->posts.post_content) - LENGTH(REPLACE(UPPER($wpdb->posts.post_content), '$q', ''))) / LENGTH('%q') AS CONTENT_CNT FROM $wpdb->posts WHERE 1=1 AND ((($wpdb->posts.post_title LIKE '%$q%') OR ($wpdb->posts.post_content LIKE '%$q%'))) AND $wpdb->posts.post_type IN ($in) AND ($wpdb->posts.post_status = 'publish' OR $wpdb->posts.post_author = 1 AND $wpdb->posts.post_status = 'private') ORDER BY IN_TITLE DESC, TITLE_CNT DESC, TITLE_POS ASC, IN_CONTENT DESC, CONTENT_CNT DESC, CONTENT_POS ASC, $wpdb->posts.post_date DESC";
-die($query);
 		$posts = $wpdb->get_results($query, OBJECT);
 		if (($c = count($posts)) > 0) {
 			if ($c > 1) {
-				$result = sprintf('%s<p class="intro-text">Your search for "%s" produced %s results, sorted by relevance.</p>', $result, $q, $c);
+				$result = sprintf('%s<p class="message">Your search for "%s" produced %s results, sorted by relevance.</p>', $result, $q, $c);
 			} else {
-				$result = sprintf('%s<p class="intro-text">Your search for "%s" produced 1 result.</p>', $result, $q);
+				$result = sprintf('%s<p class="message">Your search for "%s" produced 1 result.</p>', $result, $q);
 			}
+			$mask = apply_filters('haku_serp_date_format_filter', HAKU_SERP_DATE_FORMAT);
 			foreach ($posts as $post) {
 				$content = $post->post_content;
 				if (function_exists('markdown')) {
 					$content = markdown($content);
 				}
-				$content = wp_trim_words(strip_shortcodes($content), apply_filters('haku_excerpt_word_count_filter', HAKU_EXCERPT_WORD_COUNT));
+				$content = wp_trim_words(strip_shortcodes($content), apply_filters('haku_serp_excerpt_word_count_filter', HAKU_SERP_EXCERPT_WORD_COUNT));
 				switch (true) {
 					case ($x = strrpos($content, '.')):
 					case ($x = strrpos($content, ':')):
@@ -88,26 +89,26 @@ die($query);
 						$content = substr($content, 0, $x + 1);
 						break;
 				}
-				$url = get_permalink($post->ID);
-				$result = sprintf('%s<h2 class="entry-title"><a href="%s" rel="bookmark" >%s</a></h2>', $result, $url, $post->post_title);
-				if (apply_filters('haku_show_meta_filter', HAKU_SHOW_META)) {
-					$result = sprintf('%s<p class="entry-meta">', $result);
-					if (apply_filters('haku_show_meta_date_filter', HAKU_SHOW_META_DATE)) {
-						$result = sprintf('%s<span class="entry-time">%s</span>', $result, date(HAKU_DATE_FORMAT, strtotime($post->post_date)));
+				$permalink = get_permalink($post->ID);
+				$result = sprintf('%s<h2 class="entry-title"><a href="%s" rel="bookmark" >%s</a></h2><div class="entry-permalink">%s</div>', $result, $permalink, $post->post_title, $permalink);
+				if (apply_filters('haku_serp_show_meta_filter', HAKU_SERP_SHOW_META)) {
+					$result = sprintf('%s<div class="entry-meta">', $result);
+					if (apply_filters('haku_serp_show_meta_date_filter', HAKU_SERP_SHOW_META_DATE)) {
+						$result = sprintf('%s<span class="entry-time">%s</span>', $result, date($mask, strtotime($post->post_date)));
 					}
-					if (apply_filters('haku_show_meta_author_filter', HAKU_SHOW_META_AUTHOR)) {
-						$result = sprintf('%s by <span class="entry-author-name">%s %s</span>', $result, get_the_author_meta('first_name', $post->post_author), get_the_author_meta('last_name', $post->post_author));
+					if (apply_filters('haku_serp_show_meta_author_filter', HAKU_SERP_SHOW_META_AUTHOR)) {
+						$result = sprintf('%s by <span class="entry-author">%s %s</span>', $result, get_the_author_meta('first_name', $post->post_author), get_the_author_meta('last_name', $post->post_author));
 					}
-					$result = sprintf('%s</p>', $result);
+					$result = sprintf('%s</div>', $result);
 				}
-				$result = sprintf('%s<p class="entry-excerpt">%s <a href="%s">%s</a></p>', $result, $content, $url, __('More'));
+				$result = sprintf('%s<div class="entry-excerpt">%s</div>', $result, $content);
 			}
 		} else {
-			$result = sprintf('%s<p>Your search for "%s" produced no results.</p>', $result, $q);
+			$result = sprintf('%s<p class="message message-no-results">Your search for "%s" produced no results.</p>', $result, $q);
 		}
-	}
-	if (apply_filters('haku_show_search_form_on_results_page_filter', HAKU_SHOW_SEARCH_FORM_ON_RESULTS_PAGE)) {
-		$result = sprintf('%s%s', $result, haku_search_form(__('Search Again')));
+		if (apply_filters('haku_serp_show_form_filter', HAKU_SERP_SHOW_FORM)) {
+			$result = sprintf('%s%s', $result, haku_search_form());
+		}
 	}
 	return $result;
 }
@@ -132,7 +133,7 @@ if (strlen(get_option('permalink_structure')) > 0) {
 //
 add_action('template_redirect', 'haku_remove_post_title');
 function haku_remove_post_title() {
-	$slug = apply_filters('haku_results_page_slug_filter', HAKU_RESULTS_PAGE_SLUG);
+	$slug = apply_filters('haku_serp_slug_filter', HAKU_SERP_SLUG);
 	if (is_page($slug)) {
 		if (function_exists('genesis')) {
 			remove_action('genesis_entry_header', 'genesis_do_post_title');
@@ -148,9 +149,9 @@ function haku_remove_post_title() {
 add_filter('body_class', 'haku_body_class');
 function haku_body_class($classes) {
 	global $post;
-	$slug = apply_filters('haku_results_page_slug_filter', HAKU_RESULTS_PAGE_SLUG);
+	$slug = apply_filters('haku_serp_slug_filter', HAKU_SERP_SLUG);
 	if ($post->post_name == $slug) {
-		array_push($classes, 'haku-search-results');
+		array_push($classes, 'haku-serp');
 	}
 	return $classes;
 }
@@ -160,7 +161,7 @@ function haku_body_class($classes) {
 //
 add_action('plugins_loaded', 'haku_add_filter');
 function haku_add_filter() {
-	add_filter('get_search_form', 'haku_search_form');
+	add_filter('get_search_form', 'haku_search_form', 11);
 }
 
 ?>
