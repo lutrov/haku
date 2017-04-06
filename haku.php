0c585ad4678bda91b3c2b8980f0d68da5e0b47af
Ivan Lutrov <lutrov@users.noreply.github.com>
2017-04-06 14:58:14 +1000

    Update haku.php

--- haku.php
+++ haku.php
@@ -15,7 +15,7 @@ defined('ABSPATH') || die('Ahem.');
 // Define constants used by this plugin.
 //
 define('HAKU_SERP_DATE_FORMAT', get_option('date_format'));
-define('HAKU_SERP_EXCERPT_WORD_COUNT', 20);
+define('HAKU_SERP_EXCERPT_WORD_COUNT', 40);
 define('HAKU_SERP_SHOW_FEATURED_IMAGE', true);
 define('HAKU_SERP_SHOW_META', true);
 define('HAKU_SERP_SHOW_META_AUTHOR', true);
