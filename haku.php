f3d3529396972a0bb7a5bb5e7d96f95140b0e5e5
Ivan Lutrov <lutrov@users.noreply.github.com>
2021-03-07 14:38:41 +1100

    Update haku.php

--- haku.php
+++ haku.php
@@ -2,12 +2,26 @@
 
 /*
 Plugin Name: Haku
-Description: Improves the default Wordpress search by treating the search string as a phrase, not as individual words. It takes relevancy into account, which means it places more importance on post types that contain the phrase in the title, which contain the phrase multiple times, and the relative position of the phrase. Why this plugin name? Haku means "search" in Finnish.
 Plugin URI: https://github.com/lutrov/haku
+Description: Improves the default Wordpress search by treating the search string as a phrase, not as individual words. It takes relevancy into account, which means it places more importance on post types that contain the phrase in the title, which contain the phrase multiple times, and the relative position of the phrase. Why this plugin name? Haku means "search" in Finnish.
+Version: 4.4
 Author: Ivan Lutrov
 Author URI: http://lutrov.com/
-Version: 4.4
-Notes: This plugin provides an API to customise the default constant values. See the "readme.md" file for more.
+Copyright: 2016, Ivan Lutrov
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301, USA. Also add information on how to
+contact you by electronic and paper mail.
 */
 
 defined('ABSPATH') || die('Ahem.');
